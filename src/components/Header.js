51e788fb846b28e347142af5a7c1354bcb5dcef7
CaueFurui <cauefurui@gmail.com>
2020-07-01 01:29:16 -0300

    the game is working! =D

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'row',
-        backgroundColor: '#EEE',
+        backgroundColor: '#AAA',
         alignItems: 'center',
         justifyContent: 'space-around',
         paddingTop: 20,
